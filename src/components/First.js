9c28b1d9147fc0f7b461f5d286a5e4a2f2162d18
KunjanThakor <kunjan7634@gmail.com>
2024-02-12 11:09:45 +0530

    first commit

--- src/components/First.js
+++ src/components/First.js
@@ -4,6 +4,7 @@ const First = () => {
     const [city, setcity] = useState('Anand');
     const [weather, setweather] = useState('Anand')
     const [search, setsearch] = useState('Anand');
+    const [time, setTime] = useState(new Date());
     //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=ec19cb7701a917d19d7e8f700f3429d7
 
     useEffect(() => {
@@ -11,51 +12,82 @@ const First = () => {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ec19cb7701a917d19d7e8f700f3429d7`;
             const response = await fetch(url);
             const resjson = await response.json();
+
             //console.log(resjson);
 
             setcity(resjson.main);
             // setcity2(resjson.weather);
             // console.log(resjson.weather);
-            setweather(resjson.weather[0]);
+            if (resjson.weather && resjson.weather.length > 0) {
+                setweather(resjson.weather[0]);
+            } else {
+                setweather(null);
+            }
         }
 
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        // () => clearInterval(interval);
         fetchApi();
     }, [search]);
+    const padWithZero = (num) => {
+        return num < 10 ? "0" + num : num;
+    };
     return (
         <>
             <div className="box">
-                <div className="inputData">
-                    <input type="search" className="inputField" value={search} onChange={(event) => {
-                        setsearch(event.target.value)
-                    }} />
-                </div>
-
-
-                {!city ? (<p>No data found</p>) : (
-                    <div>
-                        <div className="info">
-                            <i className="fa-regular fa-sun"></i>
-
-                            <h2 className="location">
-                                <br></br>
-                                <span>
-                                    {search}
-                                    <br />
-                                    {weather.main}
-                                </span>
-
-                            </h2>
-                            <h1 className="temp">
-                                {city.temp} °C
-                            </h1>
-                            <h1 className="tempmin_max">
-                                min: {city.temp_min} °C | max: {city.temp_max} °C
-                            </h1>
-                        </div>
+                <div class="card">
+                    <div class="search">
+                        <input type="search" className="inputField" value={search} onChange={(event) => {
+                            setsearch(event.target.value)
+                        }} />
                     </div>
-                )}
 
-            </div>
+
+                    {!city ? (<p>No data found</p>) : (
+                        <div>
+                            <div className="info">
+                                <div className="icon">
+                                    {/* /* <i className="fa-regular fa-sun"></i>
+                                    */ }
+
+                                    {weather && weather.main && (
+                                        <img src={`https://murphyslaw.github.io/hosted-assets/weather/${weather.main.toLowerCase()}.png`} alt="Weather Icon" />
+                                    )}
+                                    {/* <img src={`https://murphyslaw.github.io/hosted-assets/weather/${weather.main.toLowerCase()}.png`} alt="icon" /> */}
+
+                                </div>
+                                <h2 className="location">
+                                    <br></br>
+                                    <span>
+                                        {search}
+                                        <br />
+                                    </span>
+                                </h2>
+                                <h1 className="temp">
+                                    {city.temp} °C
+                                </h1>
+                                <h2 className="weatherData">
+
+                                    Weather : {weather.main} | Humidity : {city.humidity}%
+
+
+                                </h2>
+                                <h1 className="tempmin_max">
+                                    min: {city.temp_min} °C | max: {city.temp_max} °C
+                                    <br />
+                                    <br />
+                                    {padWithZero(time.getDate())} / {padWithZero(time.getMonth() + 1)} / {time.getFullYear()}
+
+                                </h1>
+                            </div>
+                        </div>
+
+                    )}
+                </div>
+            </div >
 
         </>
     )
